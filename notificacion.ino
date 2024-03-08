55d7f72d07aa402873beccccc29897ebffcba29c
David González Arenas <145173380+D4V1D13AFK@users.noreply.github.com>
2024-03-08 10:20:50 -0600

    Add files via upload

---
+++ notificacion.ino
@@ -0,0 +1,55 @@
+#include <WiFi.h>    
+#include <HTTPClient.h>
+#include <UrlEncode.h>
+
+const char* ssid = "POCO M5s";
+const char* password = "UwU12345";
+
+String phoneNumber = "5214612791065";
+String apiKey = "8036142";
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
+  HTTPClient http;
+  http.begin(url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send Message to WhatsAPP
+  sendMessage("tengo hambre we!");
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
